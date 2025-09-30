d95a3882fd69e2abb9a38ea177dcb268643fd14d
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-30 13:36:30 +0900

    [Feat] chapter 2-05 레포지토리로 데이터베이스 관리하기

--- src/main/java/com/example/Question.java
+++ src/main/java/com/example/Question.java
@@ -19,7 +19,7 @@ import lombok.Setter;
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(length = 200)
     private String subject;
