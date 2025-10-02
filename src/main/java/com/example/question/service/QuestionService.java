ec72f784fb0a611d890c4b7d470f6beca938f13f
gunha0405 <dhrjsgk0405@gmail.com>
2025-10-02 14:17:02 +0900

    [Feat] chapter 3-13 검색 기능 추가하기

--- src/main/java/com/example/question/service/QuestionService.java
+++ src/main/java/com/example/question/service/QuestionService.java
@@ -46,11 +46,11 @@ public class QuestionService {
         this.questionRepository.save(q);
     }
     
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+        return this.questionRepository.findAllByKeyword(kw, pageable);
     }
     
     public void modify(Question question, String subject, String content) {
