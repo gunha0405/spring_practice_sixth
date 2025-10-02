61e11b973f12274b80561b011d7cc135125a85ad
gunha0405 <dhrjsgk0405@gmail.com>
2025-10-02 14:02:16 +0900

    [Feat] chapter 3-10 추천 기능 추가하기

--- src/main/java/com/example/answer/model/Answer.java
+++ src/main/java/com/example/answer/model/Answer.java
@@ -1,6 +1,7 @@
 package com.example.answer.model;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import com.example.question.model.Question;
 import com.example.user.model.SiteUser;
@@ -10,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,4 +36,7 @@ public class Answer {
     
     @ManyToOne
     private SiteUser author;
+    
+    @ManyToMany
+    Set<SiteUser> voter;
 }
--- src/main/java/com/example/question/model/Question.java
+++ src/main/java/com/example/question/model/Question.java
@@ -2,6 +2,7 @@ package com.example.question.model;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import com.example.answer.model.Answer;
 import com.example.user.model.SiteUser;
@@ -12,6 +13,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -40,4 +42,7 @@ public class Question {
     
     @ManyToOne
     private SiteUser author;
+    
+    @ManyToMany
+    Set<SiteUser> voter;
 }
